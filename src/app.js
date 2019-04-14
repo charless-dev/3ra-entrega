15cd3e60c7855438d9a076a5d635723ea22e7d95
charless-dev <cagudelo20@gmail.com>
2019-04-14 04:22:58 -0500

    initial deploy

--- src/app.js
+++ src/app.js
@@ -45,7 +45,7 @@ app.use(bodyParser.urlencoded({extended:false}));
 //Routes
 app.use(require('./routes/index'));
 
-mongoose.connect('mongodb://localhost:27017/cursos', {useNewUrlParser: true}, (err,res) => {
+mongoose.connect(process.env.URLDB, {useNewUrlParser: true}, (err,res) => {
 	if (err) {
 		return console.log(err);
 	}
